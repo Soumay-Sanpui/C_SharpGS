514af1011565a3847eb3103b6a333187b9fc63bf
Soumay-sanpui <somusanpui9@gmail.com>
2026-02-04 18:52:22 +0530

    feat: added new section for learning about design patterns

---
+++ DesignPattern/Program.cs
@@ -0,0 +1,8 @@
+﻿namespace  DesignPattern;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Getting started with Design Pattern in C#.");
+    }
+}
---
+++ Events/ProductStock/Product.cs
@@ -0,0 +1,26 @@
+﻿namespace C_SharpGS.Events.ProductStock;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string? ProductName { get; set; }
+    public int Stock { get; set; }
+
+    public static void RunProductStockExample()
+    {
+        Product phone = new()
+        {
+            Id = 1,
+            ProductName = "Phone",
+            Stock = 100,
+        };
+
+        InventoryService service = new(phone);
+      
+        service.LowStock += Man.SayLowStock;
+        service.ZeroStock += Man.SayZeroStock;
+        
+        Console.WriteLine(service.PurchaseProduct(100) ? "Product Bought Successfully !" : "Unable to get Product.");
+       
+    }
+}
