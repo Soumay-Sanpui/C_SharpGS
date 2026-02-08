e1b537e2cf7387af57d4341612d0cd137ae0f702
= <=>
2026-02-08 11:30:38 +0530

    feat: added Entity Framework core

---
+++ GameStoreAPI/Data/GameStoreContext.cs
@@ -0,0 +1,10 @@
+using GameStoreAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameStoreAPI.Data;
+
+public class GameStoreContext(DbContextOptions<GameStoreContext> options) : DbContext(options)
+{
+    public DbSet<Game> Games => Set<Game>();
+    public DbSet<Genre> Genres => Set<Genre>();
+}
--- GameStoreAPI/Program.cs
+++ GameStoreAPI/Program.cs
@@ -1,8 +1,12 @@
+using GameStoreAPI.Data;
 using GameStoreAPI.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 // should be added before calling .Build() method.
 builder.Services.AddValidation(); // for adding validation to all the endpoints.
+
+var ConnString = "Data Source=GameStore.db";
+builder.Services.AddSqlite<GameStoreContext>(ConnString);
 
 var app = builder.Build();
 
